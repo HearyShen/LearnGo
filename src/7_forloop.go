a4487ed95416feebb1844a1d05f27cc5315354ec
HearyShen <jiayun.shen@foxmail.com>
2020-11-20 18:04:01 +0800

    add comment that go only support for-loop

--- src/7_forloop.go
+++ src/7_forloop.go
@@ -5,6 +5,9 @@ import "fmt"
 // run with commands:
 // go run src\7_forloop.go
 
+// Golang only has for loop
+// Golang does not support while and do-while loop
+
 func main() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
