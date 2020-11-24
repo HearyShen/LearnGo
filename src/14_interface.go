f9c7eaccf0b46aa59f70bb568348d301db1d7393
HearyShen <jiayun.shen@foxmail.com>
2020-11-24 20:43:20 +0800

    learn interface in golang

---
+++ src/14_interface.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+// run with commands:
+// go run src\14_interface.go
+
+/*
+	standard interface paradigm:
+
+	type interfaceName interface {
+		func1(inputParams) (returnParams)
+		func2(inputParams) (returnParams)
+		func3(inputParams) (returnParams)
+	}
+
+	If the interfaceName is in uppercase, its a public interface.
+	If the function name is in uppercase, its a public function.
+	A public function can be accessed outside of the package,
+	otherwise, non-public function can only be accessed inside of
+	the package.
+*/
+
+type Printer interface {
+	Print(interface{})
+}
+
+type FuncCaller func(p interface{})
+
+func (funcCaller FuncCaller) Print(p interface{}) {
+	funcCaller(p)
+}
+
+func main() {
+	// Printer is the abstraction of printer
+	// FuncCaller func is the implementation of printer
+	// printer can call Printer.Print implemented by FuncCaller's Print
+	var printer Printer
+	printer = FuncCaller(func(p interface{}) {
+		fmt.Println(p)
+	})
+	// cast an anonymous function to FuncCaller type
+	// then printer calls Print implemented by FuncCaller
+	printer.Print("Golang is Good!")
+}
